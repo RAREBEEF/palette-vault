501e95df791f0c840de55fd9d9185f7b5a47493e
RAREBEEF <drrobot409@gmail.com>
2022-08-17 12:58:24 +0900

    pwa work #4

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,10 +13,20 @@ const Nav = () => {
   const checkPath = useCheckPath();
 
   useEffect(() => {
-    window.addEventListener("beforeinstallprompt", (e) => {
-      e.preventDefault();
+    // window.addEventListener("beforeinstallprompt", (e) => {
+    //   e.preventDefault();
+    //   //@ts-ignore
+    //   window.promptEvent = e;
+    //   navigate("/install", { replace: true });
+    // });
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the mini-infobar from appearing on mobile.
+      event.preventDefault();
+      console.log("👍", "beforeinstallprompt", event);
+      // Stash the event so it can be triggered later.
       //@ts-ignore
-      window.promptEvent = e;
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container.
       navigate("/install", { replace: true });
     });
   }, [navigate]);
