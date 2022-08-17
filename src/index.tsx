5f7fc132b964edddd95a6d9569dbfb13126c7f03
RAREBEEF <drrobot409@gmail.com>
2022-08-17 10:28:24 +0900

    pwa work

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,14 @@ import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
